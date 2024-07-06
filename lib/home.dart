214bc4193293af4e667f9ae3917947d2200adcc4
nayu1212 <agrawalnayan1212@gmail.com>
2024-07-06 12:40:24 +0530

    added cross icon

--- lib/home.dart
+++ lib/home.dart
@@ -11,7 +11,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final searchcontroller = TextEditingController();
-  Color bookmarkColor = Colors.green; // Initial
+  Color bookmarkColor = Colors.green;
   void toggleBookmarkColor() {
     setState(() {
       bookmarkColor = bookmarkColor == Colors.grey ? Colors.green : Colors.grey;
@@ -36,50 +36,62 @@ class _HomeState extends State<Home> {
     "Quantitative Strategy",
     "Quantitative Strategy"
   ];
+  //nayan
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.only(top: 30),
+          padding: const EdgeInsets.only(top: 41),
           child: Column(children: [
-            SizedBox(
-              height: 51,
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 50.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Color(0xFF2A2136),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: TextField(
-                    style: TextStyle(fontSize: 12.8, color: Color(0xFFEBECEC)),
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
+            Row(
+              children: [
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    child: Container(
+                      width: 300,
+                      height: 51,
+                      decoration: BoxDecoration(
+                        color: Color(0xFF2A2136),
                         borderRadius: BorderRadius.circular(10.0),
-                        borderSide: BorderSide.none,
                       ),
-                      hintText: "Search by stock Name, Patterns...",
-                      hintStyle: TextStyle(color: Colors.white54),
-                      icon: Padding(
-                        padding: const EdgeInsets.only(left: 10),
-                        child: Icon(
-                          Icons.search,
-                          color: Colors.green,
+                      child: TextField(
+                        style:
+                            TextStyle(fontSize: 12.8, color: Color(0xFFEBECEC)),
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: BorderSide.none,
+                          ),
+                          hintText: "Search by stock Name, Patterns...",
+                          hintStyle: TextStyle(color: Colors.white54),
+                          icon: Padding(
+                            padding: const EdgeInsets.only(left: 10),
+                            child: Icon(
+                              Icons.search,
+                              color: Colors.green,
+                            ),
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
+                Icon(
+                  Icons.close,
+                  color: Colors.white,
+                  size: 30,
+                )
+              ],
             ),
             SizedBox(
-              height: 30,
+              height: 20,
             ),
             Row(
               children: [
                 Padding(
-                  padding: const EdgeInsets.only(left: 20),
+                  padding: const EdgeInsets.only(left: 15),
                   child: Container(
                     child: Center(
                         child: Text("All",
@@ -109,7 +121,7 @@ class _HomeState extends State<Home> {
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(left: 20),
+                  padding: const EdgeInsets.only(left: 15),
                   child: Container(
                     child: Center(
                         child: Text("Strategies",
@@ -141,7 +153,7 @@ class _HomeState extends State<Home> {
               ],
             ),
             SizedBox(
-              height: 30,
+              height: 20,
             ),
             Expanded(
               child: ListView.builder(
@@ -157,7 +169,7 @@ class _HomeState extends State<Home> {
                               height: 35,
                               decoration: BoxDecoration(
                                 color: Colors.white,
-                                borderRadius: BorderRadius.circular(10.0),
+                                borderRadius: BorderRadius.circular(8.0),
                               ),
                             ),
                             title: Text(
